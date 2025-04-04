552ee0c123a7c891d8cbea217952b1ba508c2cda
ms-Ana <ms.urmanana@gmail.com>
2025-04-04 13:06:50 +0300

    add visualization

---
+++ code/models/landmark/visualization/utils2d.py
@@ -0,0 +1,33 @@
+POSE_CONNECTIONS = [
+    (0, 1),
+    (1, 2),
+    (2, 3),
+    (3, 7),  # left eye and ear
+    (0, 4),
+    (4, 5),
+    (5, 6),
+    (6, 8),  # right eye and ear
+    (9, 10),  # mouth
+    (11, 12),  # shoulders
+    (11, 13),
+    (13, 15),  # left arm
+    (12, 14),
+    (14, 16),  # right arm
+    (15, 17),
+    (15, 19),
+    (15, 21),  # left wrist to fingers
+    (16, 18),
+    (16, 20),
+    (16, 22),  # right wrist to fingers
+    (11, 23),
+    (12, 24),  # torso sides
+    (23, 24),  # hips
+    (23, 25),
+    (25, 27),
+    (27, 29),
+    (29, 31),  # left leg
+    (24, 26),
+    (26, 28),
+    (28, 30),
+    (30, 32),  # right leg
+]
