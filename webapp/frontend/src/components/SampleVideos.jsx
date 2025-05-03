d7df349b71cc61830bc3399f7f1b8201d1637ace
patrickf949 <43245704+patrickf949@users.noreply.github.com>
2025-05-03 15:26:47 +0300

    configure sample videos component to work well with the video player

--- webapp/frontend/src/components/SampleVideos.jsx
+++ webapp/frontend/src/components/SampleVideos.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState,useRef } from 'react';
+import { useEffect, useState, useRef } from "react";
 
 import {
   Box,
@@ -9,8 +9,9 @@ import {
   Grid,
   CircularProgress,
   IconButton,
-} from '@mui/material';
-import { ArrowBackIos, ArrowForwardIos } from '@mui/icons-material';
+} from "@mui/material";
+import { ArrowBackIos, ArrowForwardIos, CheckBox } from "@mui/icons-material";
+import { useTranslationStore } from "@/store/translationStore";
 
 // Utility to extract Drive File ID
 const extractDriveFileId = (url) => {
@@ -22,32 +23,30 @@ const extractDriveFileId = (url) => {
 const fetchVideos = async () => {
   return [
     {
-      url: 'https://drive.google.com/file/d/1cT39EVXn-S0lOY_YrF62YC2I5OZWiJNQ/view?usp=drive_link',
-      title: 'Sample Video 1',
-      label:'comer_uf_3.mp4',
-
+      url: "https://drive.google.com/file/d/1cT39EVXn-S0lOY_YrF62YC2I5OZWiJNQ/view?usp=drive_link",
+      title: "Sample Video 1",
+      label: "comer_uf_3.mp4",
     },
     {
-      url: 'https://drive.google.com/file/d/1m3wDaqv2kOai9mq_ZCHkbthTdVTNOb2x/view?usp=sharing',
-      title: 'Sample Video 2',
-      label:'casa_vl_4.mp4',
+      url: "https://drive.google.com/file/d/1m3wDaqv2kOai9mq_ZCHkbthTdVTNOb2x/view?usp=sharing",
+      title: "Sample Video 2",
+      label: "casa_vl_4.mp4",
     },
     {
-     url: 'https://drive.google.com/file/d/1vQGyP3820sJokH7qA4iPK5YI6bijT3tb/view?usp=drive_link',
-        title: 'Sample Video 3',
-        label:'cabeça_sb_2.mp4',
+      url: "https://drive.google.com/file/d/1vQGyP3820sJokH7qA4iPK5YI6bijT3tb/view?usp=drive_link",
+      title: "Sample Video 3",
+      label: "cabeça_sb_2.mp4",
     },
     {
-      url: 'https://drive.google.com/file/d/1YliBNPaGb59qb9iHbm0iT53gFuYOFper/view?usp=drive_link',
-      title: 'Sample Video 4',
-      label:'ajudar_ne_1.mp4'
+      url: "https://drive.google.com/file/d/1YliBNPaGb59qb9iHbm0iT53gFuYOFper/view?usp=drive_link",
+      title: "Sample Video 4",
+      label: "ajudar_ne_1.mp4",
     },
-  
   ];
 };
 
-
 const VideoLibrary = () => {
+  const { setVideo,resetVideo, setLoadingVideo } = useTranslationStore();
   const [videos, setVideos] = useState([]);
   const [playingId, setPlayingId] = useState(null);
   const scrollRef = useRef();
@@ -55,7 +54,15 @@ const VideoLibrary = () => {
   const [showRight, setShowRight] = useState(false);
 
   useEffect(() => {
-    fetchVideos().then(setVideos);
+
+    fetchVideos().then((response)=>{
+      const videos = response.map((video)=>{
+        video['id'] = extractDriveFileId(video.url);
+        video['videoUrl'] = `https://drive.google.com/uc?id=${video.id}`;
+        return video;
+      })
+      setVideos(videos);
+    });
   }, []);
 
   useEffect(() => {
@@ -68,12 +75,12 @@ const VideoLibrary = () => {
 
     const ref = scrollRef.current;
     if (ref) {
-      ref.addEventListener('scroll', checkScroll);
+      ref.addEventListener("scroll", checkScroll);
       checkScroll(); // run initially
     }
 
     return () => {
-      if (ref) ref.removeEventListener('scroll', checkScroll);
+      if (ref) ref.removeEventListener("scroll", checkScroll);
     };
   }, [videos]);
 
@@ -81,17 +88,33 @@ const VideoLibrary = () => {
 
   const scroll = (dir) => {
     if (!scrollRef.current) return;
-    scrollRef.current.scrollBy({ left: dir * scrollByAmount, behavior: 'smooth' });
+    scrollRef.current.scrollBy({
+      left: dir * scrollByAmount,
+      behavior: "smooth",
+    });
   };
+  const handleVideo = (video) => {
+    if (playingId === video.id) {
+      resetVideo();
+      
+    }else{
+      
+      setVideo({
+        videoUrl: video.id,
+        video: null,
+        label: video.label,
+      });
+    }
+  }
 
   const handlePlay = (id) => {
     setPlayingId((prev) => (prev === id ? null : id));
   };
 
   return (
-    <Box sx={{ p: 3, position: 'relative' }}>
+    <Box sx={{ p: 3, position: "relative" }}>
       <Typography variant="h7" gutterBottom>
-        Video Library
+        Sample Sign Language Videos
       </Typography>
 
       {videos.length === 0 ? (
@@ -99,29 +122,31 @@ const VideoLibrary = () => {
           <CircularProgress />
         </Box>
       ) : (
-        <Box sx={{ position: 'relative' }}>
+        <Box sx={{ position: "relative" }}>
           {showLeft && (
             <IconButton
               onClick={() => scroll(-1)}
               sx={{
-                position: 'absolute',
-                top: '50%',
+                position: "absolute",
+                top: "50%",
                 left: 0,
-                transform: 'translateY(-50%)',
+                transform: "translateY(-50%)",
                 zIndex: 1,
 
-                background: 'white',
+                background: "white",
                 boxShadow: 1,
-                alignContent: 'center',
-                justifyContent: 'center',
-                display: 'flex',
-                textAlign: 'center',
+                alignContent: "center",
+                justifyContent: "center",
+                display: "flex",
+                textAlign: "center",
               }}
             >
-              <ArrowBackIos sx={{
-                marginLeft:'auto',
-                marginRight:'auto',
-                }} />
+              <ArrowBackIos
+                sx={{
+                  marginLeft: "auto",
+                  marginRight: "auto",
+                }}
+              />
             </IconButton>
           )}
 
@@ -129,16 +154,16 @@ const VideoLibrary = () => {
             <IconButton
               onClick={() => scroll(1)}
               sx={{
-                position: 'absolute',
-                top: '50%',
+                position: "absolute",
+                top: "50%",
                 right: 0,
-                transform: 'translateY(-50%)',
+                transform: "translateY(-50%)",
                 zIndex: 1,
-                background: 'white',
+                background: "white",
                 boxShadow: 1,
-                alignContent: 'center',
-                justifyContent: 'center',
-                display: 'flex',
+                alignContent: "center",
+                justifyContent: "center",
+                display: "flex",
               }}
             >
               <ArrowForwardIos />
@@ -148,12 +173,12 @@ const VideoLibrary = () => {
           <Box
             ref={scrollRef}
             sx={{
-              overflowX: 'auto',
-              display: 'flex',
+              overflowX: "auto",
+              display: "flex",
               gap: 2,
-              scrollSnapType: 'x mandatory',
+              scrollSnapType: "x mandatory",
               pb: 1,
-              scrollBehavior: 'smooth',
+              scrollBehavior: "smooth",
             }}
           >
             {videos.map((video) => {
@@ -164,41 +189,54 @@ const VideoLibrary = () => {
                 <Grid
                   key={id}
                   sx={{
-                    flex: '0 0 calc(50% - 16px)',
-                    scrollSnapAlign: 'start',
+                    flex: "0 0 calc(50% - 16px)",
+                    scrollSnapAlign: "start",
                   }}
                 >
-                  <Card onClick={() => handlePlay(id)} sx={{ cursor: 'pointer' }}>
+                  <Card
+                    onClick={() => {
+                      setLoadingVideo(true);
+                      handlePlay(id);
+                      handleVideo(video);
+                      setLoadingVideo(false);
+                    }}
+                    sx={{ cursor: "pointer" }}
+                  >
+                    <CardMedia
+                      component="img"
+                      sx ={{
+                        height:124
+                      }}
+                      image={`https://drive.google.com/thumbnail?id=${id}`}
+                      alt={video.title}
+                    />
                     {!isPlaying ? (
                       <>
-                        <CardMedia
-                          component="img"
-                          height="180"
-                          image={`https://drive.google.com/thumbnail?id=${id}`}
-                          alt={video.title}
-                        />
                         <CardContent>
-                          <Typography variant="subtitle1">{video.title}</Typography>
+                          <Typography variant="subtitle1">
+                            {video.title}
+                          </Typography>
                           <Typography variant="body2" color="text.secondary">
                             Click to play
                           </Typography>
                         </CardContent>
                       </>
                     ) : (
-                      <Box>
-                         
-                        <iframe
-                            src={`https://drive.google.com/file/d/${id}/preview`}
-                            width="100%"
-                            allow="autoplay"
-                            autoPlay
-                            style={{ borderRadius: 8 }}
-                            allowFullScreen
-                        ></iframe>
-                        <CardContent>
-                          <Typography variant="subtitle1">{video.title}</Typography>
+                      <Box sx={{ fontWeight: "bolder" }}>
+                        <CardContent
+                          sx={{
+                            borderBottom: "1px solid green",
+                            borderRight: "1px solid green",
+                            borderLeft: "1px solid green",
+                            borderRadius: "0px 0px 4px 4px", 
+                          }}
+                        >
+                          <Typography color="text.primary" variant="subtitle1">
+                            <CheckBox />
+                            {video.title}
+                          </Typography>
                           <Typography variant="body2" color="text.secondary">
-                            Click again to close
+                            Click again to deselect
                           </Typography>
                         </CardContent>
                       </Box>
