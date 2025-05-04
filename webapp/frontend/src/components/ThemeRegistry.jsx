2eafb970ff05a8ef6065bde6c74a8e9a64b7e1ec
patrickf949 <43245704+patrickf949@users.noreply.github.com>
2025-05-04 12:35:38 +0300

    move page components and styling to containers folder to fix hydration error

--- webapp/frontend/src/components/ThemeRegistry.jsx
+++ webapp/frontend/src/components/ThemeRegistry.jsx
@@ -5,11 +5,13 @@ import { CacheProvider } from '@emotion/react';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import createEmotionCache from '../lib/emotionCache';
 import { createTheme } from '@mui/material/styles';
+import { useState } from 'react';
 
 const theme = createTheme(); // You can customize this
-const emotionCache = createEmotionCache();
+
 
 export default function ThemeRegistry({ children }) {
+  const [emotionCache] = useState(()=>createEmotionCache());
   return (
     <CacheProvider value={emotionCache}>
       <ThemeProvider theme={theme}>
