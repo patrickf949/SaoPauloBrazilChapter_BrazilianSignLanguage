6f8267f7885f1218208a658a9309873685b22a91
patrickf949 <43245704+patrickf949@users.noreply.github.com>
2025-08-08 09:17:41 +0300

    feat(backend): introduce services module
    - add a new services module to house business logic
    - this module will contain services for prediction, video processing, and more

---
+++ webapp/backend/services/__init__.py
@@ -0,0 +1,22 @@
+from .analyzer import VideoAnalyzerService
+from .preprocessor import PreprocessorService
+from .prediction import PredictionService  
+from .results import ResultsSaver
+from .metadata import MetadataFilterService, MetadataExtractor
+from .video_saver import VideoSaver
+from .video_validator import VideoValidator
+from .landmark import LandmarkDrawerService
+from .pipeline import FeaturePipeline
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ webapp/backend/services/model_references.py
@@ -0,0 +1,29 @@
+
+# Add /code modules dir to sys.path so imports work
+from pathlib import Path
+import sys
+
+
+current_dir = Path(__file__).resolve().parent.parent
+root_dir = current_dir.parent.parent
+
+# ------------------------------------------------------------
+# if /webapp doesn't need to be standalone, uncomment this line to import directly from /code:
+code_dir = root_dir / 'code'
+# ------------------------------------------------------------
+# if /webapp should be standalone, run sync_code.py script, and uncomment this line:
+# code_dir = current_dir / 'shared_code'
+
+if str(code_dir) not in sys.path:
+    sys.path.insert(0, str(code_dir))
+
+# Import /code modules. (Type ignore comments for Pylance)
+from data.download_videos import get_video_metadata # type: ignore
+from preprocess.video_analyzer import VideoAnalyzer # type: ignore
+from preprocess.preprocessor import Preprocessor # type: ignore
+from preprocess.vizualisation import draw_landmarks_on_video_with_frame # type: ignore
+from model.features.feature_processor import FeatureProcessor # type: ignore
+from model.utils.inference import InferenceEngine # type: ignore
+from model.utils.utils import load_obj # type: ignore
+from model.dataset.frame_sampling import get_sampling_function # type: ignore
+# Import /code modules. (Type ignore comments for Pylance)
