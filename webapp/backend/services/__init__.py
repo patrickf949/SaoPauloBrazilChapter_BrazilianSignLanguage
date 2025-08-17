83c9f765e48e42c8279ef9de1d715d2b5a542d41
patrickf949 <43245704+patrickf949@users.noreply.github.com>
2025-08-17 10:25:14 +0300

    refactor: update video_saver imports
    - updated video_saver imports to use RequestVideoSaver and LabelVideoSaver

--- webapp/backend/services/__init__.py
+++ webapp/backend/services/__init__.py
@@ -3,7 +3,7 @@ from .preprocessor import PreprocessorService
 from .prediction import PredictionService  
 from .results import ResultsSaver
 from .metadata import MetadataFilterService, MetadataExtractor
-from .video_saver import VideoSaver
+from .video_saver import RequestVideoSaver, LabelVideoSaver
 from .video_validator import VideoValidator
 from .landmark import LandmarkDrawerService
 from .pipeline import FeaturePipeline
