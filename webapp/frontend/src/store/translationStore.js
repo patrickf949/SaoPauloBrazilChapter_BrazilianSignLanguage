9965bc0d6df5f23acc0868f3402a363118f19fc4
patrickf949 <43245704+patrickf949@users.noreply.github.com>
2025-08-17 09:26:34 +0300

    feat: update translation store
    - add resetResult function
    - update resetVideo function
    - fix setResult function

--- webapp/frontend/src/store/translationStore.js
+++ webapp/frontend/src/store/translationStore.js
@@ -1,21 +1,24 @@
-import { create } from 'zustand';
+import { create } from "zustand";
 
 export const useTranslationStore = create((set) => ({
   info: {
-    videoUrl: '',
+    videoUrl: "",
     video: null,
     label: null,
   },
   loading: false,
   loadingVideo: false,
   result: {
-    video:null,
-    videoUrl:null,
-    label:null,
+    video: null,
+    videoUrl: null,
+    label: null,
   },
+
+  // State setters
   setLoading: (payload) => set(() => ({ loading: payload })),
   setVideo: (payload) => set(() => ({ info: payload })),
-  setResult: (payload) => set(()=> ({result})),
-  resetVideo:()=> set(() => ({ info: { label: '', video: null}})),
-  setLoadingVideo: (payload) => set(() => ({ loadingVideo: payload })), 
+  setResult: (payload) => set(() => ({ result: payload })),
+  resetVideo: () => set(() => ({ info: { label: "", video: null, videoUrl: "" } })),
+  resetResult: () => set(() => ({ result: { label: null, video: null, videoUrl: null } })),
+  setLoadingVideo: (payload) => set(() => ({ loadingVideo: payload })),
 }));
