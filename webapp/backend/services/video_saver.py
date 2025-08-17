0cce708f62c837db8f0a7e945e46aae4007c2964
patrickf949 <43245704+patrickf949@users.noreply.github.com>
2025-08-17 10:25:32 +0300

    refactor: split video saver into request and label savers
    - renamed VideoSaver to RequestVideoSaver
    - added LabelVideoSaver for copying existing videos
    - added error handling for video saving

--- webapp/backend/services/video_saver.py
+++ webapp/backend/services/video_saver.py
@@ -1,11 +1,24 @@
 
 from pathlib import Path
-from fastapi import UploadFile
+from fastapi import HTTPException, UploadFile, status
 
-from utils import save_uploaded_file
+from utils import copy_existing_video, save_uploaded_file
 
 
-class VideoSaver:
+class RequestVideoSaver:
     @staticmethod
     async def save(file: UploadFile, interim_dir: Path) -> Path:
         return await save_uploaded_file(file, interim_dir)
+    
+
+class LabelVideoSaver:
+    @staticmethod
+    async def save(file: Path, interim_dir: Path) -> Path:
+        try:
+
+            return await copy_existing_video(file, interim_dir)
+        except Exception as error:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=f"Choose a valid video: {error}"
+            )
