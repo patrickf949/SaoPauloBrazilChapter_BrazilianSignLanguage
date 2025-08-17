4b2e2df999856c1e44abaa2b7ea27d3f79c18167
patrickf949 <43245704+patrickf949@users.noreply.github.com>
2025-08-17 09:25:45 +0300

    feat: update results component
    - add loader and video loader
    - improve result display ui
    - add loading state

--- webapp/frontend/src/components/Results.jsx
+++ webapp/frontend/src/components/Results.jsx
@@ -1,8 +1,13 @@
 import { useTranslationStore } from "@/store/translationStore";
 import { Box, Typography } from "@mui/material";
+import Loader, { VideoLoader } from "./Loader";
+import { useEffect } from "react";
 
 const Results = () => {
   const { loading, result } = useTranslationStore();
+  useEffect(() => {
+      console.log({result});
+  },[result]);
   return (
     <Box sx={{ padding: 2, height: "100%", textAlign: "center" }}>
       <Typography variant="h5">Results</Typography>
@@ -15,9 +20,13 @@ const Results = () => {
             alignItems: "center",
           }}
         >
-          <Typography variant="subtitle1">
+          {!result?.label ?<Typography variant="subtitle1">
             The interpretation of your selected video will appear here.
           </Typography>
+          :
+          <Typography variant="subtitle1">
+            {result?.label}
+          </Typography>}
         </Box>
       )}
       {loading && (
@@ -29,7 +38,8 @@ const Results = () => {
             alignItems: "center",
           }}
         >
-          <Typography variant="h5">Waiting for results...</Typography>
+          <Typography variant="h5">Loading Results...</Typography>
+          <Loader />
         </Box>
       )}
       {result && (
@@ -41,9 +51,12 @@ const Results = () => {
           }}
         >
           <hr />
-          <video
+          {result?.videoUrl ?<video
             controls
-            src={result?.videoUrl}
+            src={result.videoUrl}
+            autoPlay
+            muted
+            loop
             height={300}
             style={{
               borderRadius: 8,
@@ -55,9 +68,37 @@ const Results = () => {
               marginLeft: "auto",
               marginRight: "auto",
             }}
-          />
+          />:
+          <div
+            width="100%"
+            height={300}
+            style={{
+              background: "#414141ff",
+              borderRadius: 8,
+              marginTop: "0.5rem",
+              maxWidth: "100%",
+              height: 300,
+            }}
+          />}
+
+          {loading && (
+            <div
+              style={{
+                position: "relative",
+                top: -240,
+                left: 0,
+                right: 0,
+                bottom: 0,
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <VideoLoader />
+            </div>
+          )}
           <hr />
-          <Typography variant="h6">{result?.label}</Typography>
+          <Typography variant="h5" sx={{ color: "success.main", fontWeight: 500 }}>Result: {result?.label}</Typography>
         </Box>
       )}
     </Box>
