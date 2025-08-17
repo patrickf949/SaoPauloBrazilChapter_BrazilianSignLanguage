5bc1bbf579d3029a1aaf53eb2c8c1ca984281267
patrickf949 <43245704+patrickf949@users.noreply.github.com>
2025-08-17 09:25:26 +0300

    feat: update interpretation api
    - change endpoint to /predict/
    - set content-type to multipart/form-data

--- webapp/frontend/src/api/interprete.js
+++ webapp/frontend/src/api/interprete.js
@@ -2,12 +2,14 @@
 This file contains the API calls for the interprete service.
 It uses axios to make HTTP requests to the backend service.
 */
-import axios from 'axios';
-import { API_URL } from '@/constants';
+import axios from "axios";
+import { API_URL } from "@/constants";
 
-const getInterpretation = async (data)=>{
-    const response = await axios.post(`${API_URL}/translate`, data,);
-    return response;
-}
+const getInterpretation = async (data) => {
+  const response = await axios.post(`${API_URL}/predict/`, data, {
+    headers: { "Content-Type": "multipart/form-data" },
+  });
+  return response;
+};
 
 export { getInterpretation };
